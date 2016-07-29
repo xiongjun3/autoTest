825fda7656a77f47954b94ac6e6f0a0520a6549d
xiongjun3 <412882383@qq.com>
2016-07-29 17:51:38 +0800

    more case

---
+++ https.py
@@ -0,0 +1,27 @@
+#coding=utf-8
+
+import requests
+import json
+
+def getSmsCode(tel):
+	res = requests.get("https://slack.com/api/channels.history?token=xoxp-2243966434-52358629782-58614973893-8c4b7e98f3&channel=C0CB3ARKP&count=5&pretty=1")
+	#将https请求的返回值存入res
+	json_str = res.content   #获取返回值
+
+	datas = json.loads(json_str)   #解析js
+	sms = datas['messages']       
+
+	code = ""
+
+	for item in sms:
+	  if item.has_key("username") == False or item["username"] != "SMS":
+	    continue
+	  if item.has_key("text") == True and item["text"].find(tel) >= 0:
+	  	code = item["text"][(item["text"].find("code:") + 6):]
+	  	break
+
+	return code
+
+
+print getSmsCode("18811145064") 
+  
--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 
-#coding=utf-8
+#-*- coding:utf-8 -*-
+
 
 import os
 #from selenium import webdriver
@@ -7,6 +8,7 @@ from appium import webdriver
 import time
 import unittest
 import json
+import https
 
 config_file = file("./config.json")
 config = json.load(config_file)
@@ -33,6 +35,9 @@ class DpAppTests(unittest.TestCase):
 		desired_caps['app'] = PATH(config['app'])
 		desired_caps['appPackage'] = 'com.liwushuo.gifttalk'  #包名
 		desired_caps['appActivity'] = 'com.liwushuo.gifttalk.MainActivity'
+		desired_caps['unicodeKeyboard'] = True
+		desired_caps['resetKeyboard'] = True
+
 
 		self.driver = webdriver.Remote('http://localhost:4723/wd/hub',desired_caps)
 
@@ -41,25 +46,91 @@ class DpAppTests(unittest.TestCase):
 
 	def test_dpApp(self):  #需要执行的case
 
-		time.sleep(2)
+		#登陆
+		time.sleep(10)
 		self.driver.find_element_by_xpath("//android.widget.TextView[contains(@text,'我的')]").click()
-
 		self.driver.find_element_by_xpath("//android.widget.TextView[contains(@text,'未登录')]/preceding-sibling::*[1]").click()
-		
 
-		#self.login_with_uname()
+		self.login_with_psw()
 		#self.login_with_code()
-		self.login_with_QQ()
+		#self.login_with_QQ()
+		#self.login_with_wechat()
+		#self.login_with_weibo()
 
 		time.sleep(5)
 
-	def login_with_uname(self):
+		#修改密码
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/avatar").click()
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/rl_set_password").click()
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/et_pwd").send_keys("qwerty")
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/action_open_regist").click()
+
+		#断言
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/logout").click()
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/avatar").click()
+		self.driver.find_element_by_xpath("//android.widget.EditText[contains(@text,'输入手机号')]").send_keys('17710144548')	
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/et_login_certificate").send_keys('qwerty')
+
+		self.driver.find_element_by_xpath("//android.widget.Button[contains(@text,'登录')]").click()		
+		try:
+			user_name = self.driver.find_element_by_id("com.liwushuo.gifttalk:id/nickname").text
+			assert (user_name == "17710144548"),"login fail"
+		except AssertionError,msg:
+			print msg
+
+		#还原密码
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/avatar").click()
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/rl_set_password").click()
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/et_pwd").send_keys("123456")
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/action_open_regist").click()
+
+		#添加地址
+		address_count_before = self.driver.find_element_by_id("com.liwushuo.gifttalk:id/address_count").text
+
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/rl_address").click()
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/build_new_address").click()
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/et_receiving_name").send_keys("auto_name1")
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/et_receiving_phone").send_keys("13511111111")
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/tx_pcd").click()
+		time.sleep(2)
+
+		#滑动选省市区
+		#province = self.driver.find_element_by_id("com.liwushuo.gifttalk:id/province")
+
+
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/done").click()
+
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/et_road").set_text("中文地址")
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/right_text").click()
+		address_count_after = self.driver.find_element_by_id("com.liwushuo.gifttalk:id/address_count").text
+		self.assertEqual(address_count_before,address_count_after,msg="creat new address fail!")
+
+
+
+		#删掉地址
+		self.driver.find_element_by_xpath("//android.widget.TextView[contains(@text,'收货人：auto_name1')]").click()
+		self.driver.find_element_by_xpath("//android.widget.TextView[contains(@text,'删除')]").click()
+		time.sleep(1)
+		self.driver.find_element_by_id("android:id/button1").click()
+		address_count_after_delete ＝ self.driver.find_element_by_id("com.liwushuo.gifttalk:id/address_count").text
+		self.assertEqual(address_count_after_delete,address_count_after,msg="delete new address fail!")
+		self.assertNotEqual(address_count_after_delete,address_count_before,msg="delete new address fail!")
+
+
+
+	def login_with_psw(self):
 
 		self.driver.find_element_by_xpath("//android.widget.EditText[contains(@text,'输入手机号')]").send_keys('17710144548')	
 		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/et_login_certificate").send_keys('123456')
 
 		self.driver.find_element_by_xpath("//android.widget.Button[contains(@text,'登录')]").click()
-	
+
+		try:
+			user_name = self.driver.find_element_by_id("com.liwushuo.gifttalk:id/nickname").text
+			assert (user_name == "17710144548"),"login fail"
+		except AssertionError, msg:
+			print msg
+		
 
 	def login_with_code(self):
 
@@ -67,16 +138,56 @@ class DpAppTests(unittest.TestCase):
 		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/tv_login_type_switch").click()
 		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/tv_auth_code_btn").click()
 		time.sleep(5)
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/et_login_certificate").send_keys(getSmsCode("17710144548"))
 		self.driver.find_element_by_xpath("//android.widget.Button[contains(@text,'登录')]").click()
+		
+		user_name = self.driver.find_element_by_id("com.liwushuo.gifttalk:id/nickname").text
+		self.assertNotEqual(user_name,"17710144548",msg="login fail")
+
+		'''
+		try:
+			user_name = self.driver.find_element_by_id("com.liwushuo.gifttalk:id/nickname").text
+			assert (user_name == "17710144548"),"login fail"
+		except AssertionError as msg:
+			print msg
+		'''
 
 
-	def login_with_QQ():
+
+	def login_with_QQ(self):
 		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/signin_qq").click()
+		time.sleep(5)
+		self.driver.find_element_by_class_name("android.widget.Button").click()
+
+		#绑定手机号
+		time.sleep(5)
+		self.driver.find_element_by_xpath("//android.widget.EditText[contains(@text,'手机号')]").send_keys('17710144548')
+		self.driver.find_element_by_xpath("//android.widget.TextView[contains(@text,'获取验证码')]").click()
+		time.sleep(5)
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/et_sms_auth_code").send_keys(https.getSmsCode("17710144548"))
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/tv_next_btn").click()
+		
+
+		#修改绑定的手机号
+
+
+
+
+	def login_with_wechat(self):
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/signin_wechat").click()
 		
 
 
-		
+	def login_with_weibo(self):
+		self.driver.find_element_by_id("com.liwushuo.gifttalk:id/signin_weibo").click()
+		time.sleep(5)
+
+
+
+										
+
 if __name__ == '__main__':
 	suite = unittest.TestLoader().loadTestsFromTestCase(DpAppTests)
-	unittest.TextTestRunner(verbosity=2).run(suite) #执行测试用例
+	runner = unittest.TextTestRunner(verbosity=2)
+	runner.run(suite) #执行测试用例
 

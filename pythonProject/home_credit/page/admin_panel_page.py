87dec55e5f964f4e8e3bfa9e91de772e7cfc22d7
xiongjun3 <412882383@qq.com>
2021-11-24 10:15:57 +0800

    先禁掉上传文件的case

--- pythonProject/home_credit/page/admin_panel_page.py
+++ pythonProject/home_credit/page/admin_panel_page.py
@@ -9,7 +9,7 @@ from home_credit.page.policy_list_page import PolicyPage
 class AdminPanelPage(BasePage):
     def goto_homecredit(self):
         locator = (By.XPATH,'//*[@class="ant-space ant-space-horizontal ant-space-align-center platforms___27-_C"]/div[3]/div/div')
-        WebDriverWait(self.driver, 5, 0.5).until(expected_conditions.element_to_be_clickable(locator))
+        WebDriverWait(self.driver, 10, 0.5).until(expected_conditions.element_to_be_clickable(locator))
         self.driver.find_element(By.XPATH,'//*[@class="ant-space ant-space-horizontal ant-space-align-center platforms___27-_C"]/div[3]/div/div').click()
         return PolicyPage(self.driver)
 
